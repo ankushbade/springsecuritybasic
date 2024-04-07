27a0848c5777b0b8ba1db53c3ef397e082954a24
ankushbade <ankush21bade@gmail.com>
2024-04-07 15:21:48 +0530

    initial commit

---
+++ src/main/java/com/example/bank/controller/WelcomeComtroller.java
@@ -0,0 +1,15 @@
+package com.example.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeComtroller 
+{
+	@GetMapping("/welcome")
+	public String welcome() 
+	{
+		return "Welcome to spring boot  without security";
+		
+	}
+}
